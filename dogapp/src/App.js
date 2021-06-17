50acfe51f99bf5e5f0b7c7ec9767f744dc60f4fb
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-06-17 16:33:57 +0100

    initalise react

---
+++ dogapp/src/App.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  /*const [err, setErr] = useState(null);
+  const [Loaded, setLoaded] = useState(false);
+  const [dogs, setDogs] = useState([]);
+  
+  useEffect()
+
+*/
+  return (
+    <div className="App">
+        <img src={} />
+        <button>Click for new picture</button>
+    </div>
+  );
+}
+
+export default App;
