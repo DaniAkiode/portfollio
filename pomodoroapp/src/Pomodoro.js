578893672d4371bfa88874a90b34263eb409dbbd
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-07-06 18:28:14 +0100

    sorting ou JSX and CSS

---
+++ pomodoroapp/src/Pomodoro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function pomodoro() {
+    return (
+        <div className="pomodoro">
+            <div className="message">
+                <div>Break time! New session starts in:</div>
+            </div>
+            <div className="timer">25:00</div>
+        </div>
+    )
+}
+
+
+export default pomodoro;
